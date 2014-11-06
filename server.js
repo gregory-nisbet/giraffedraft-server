e766423bea50280bc420866657ba30415d4847ac
Warren Tang <warranty6@gmail.com>
2014-11-06 15:47:55 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
-var routes = require('./config/routes');
+var routes = require('./server/config/routes');
+
+var port = process.env.PORT || 9001;
 
 var app = express();
 routes(app, express);
 
-app.listen(9001, function () {
+app.listen(port, function () {
 	console.log("now listening on ports over 9000.")
 });
