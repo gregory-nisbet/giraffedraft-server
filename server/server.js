6dd338992409fe47dfeff4f24430e7b88e05543f
Warren Tang <warranty6@gmail.com>
2014-11-06 15:49:55 -0800

    Delete server.js

--- server/server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var routes = require('./config/routes');
-
-var app = express();
-routes(app, express);
-
-app.listen(9001, function () {
-	console.log("now listening on ports over 9000.")
-});
