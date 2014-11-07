4c682624b3998bd53b823c16d82785642212207e
concreted <arichuang@gmail.com>
2014-11-06 20:00:30 -0800

    Add CORS headers to routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -6,6 +6,12 @@ module.exports = function (app, express) {
 	// This allows client/index.html to be used on the / route.
 	app.use(express.static(__dirname + '/../../client'));
 
+	app.all('/', function(req, res, next) {
+		res.header("Access-Control-Allow-Origin", '*');
+		res.header("Access-Control-Allow-Headers", "X-Requested-With");
+		next();
+	});
+
 	app.get("/api", function (req, res) {
 		res.sendFile(path.resolve(__dirname + "/../data/data.json"));
 	});
