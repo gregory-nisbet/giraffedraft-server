6273ae8dd34319d8eb043a4ad02c406ed18f4830
concreted <arichuang@gmail.com>
2014-11-06 19:29:04 -0800

    Added basic tests

---
+++ test/tests.js
@@ -0,0 +1,35 @@
+var mocha = require('mocha');
+var assert = require('chai').assert;
+var request = require('supertest');
+
+var app = require('../server.js');
+
+describe('GET /', function() {
+  it('should return html', function(done) {
+    request(app)
+      .get('/')
+      .set('Accept', 'application/html')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        }
+        done();
+      });
+  });
+});
+
+describe('GET /api', function() {
+  it('should return JSON', function(done) {
+    request(app)
+      .get('/api')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        }
+        done();
+      });
+  });
+});
