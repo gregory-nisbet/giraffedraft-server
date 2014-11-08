3fbc7e52d0426d8f16f5142d2a52eb4658a6f80a
pouncealot <spamtoheck@gmail.com>
2014-11-07 19:48:14 -0800

    fixed cors

--- server/config/routes.js
+++ server/config/routes.js
@@ -10,7 +10,8 @@ module.exports = function (app, express) {
 
 	app.use(function(req, res, next) {
 		res.header("Access-Control-Allow-Origin", '*');
-		res.header("Access-Control-Allow-Headers", "X-Requested-With");
+		res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+		res.header("Access-Control-Allow-Headers",  'Content-Type');
 		next();
 	});
 
